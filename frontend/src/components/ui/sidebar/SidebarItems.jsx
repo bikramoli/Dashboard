542a1169719a3a33ecc67df1cde99131cf4c39bf
bikram <bikram@dibtech.com.au>
2022-11-02 22:51:23 +0545

    sidebar item list

---
+++ frontend/src/components/ui/sidebar/SidebarItems.jsx
@@ -0,0 +1,43 @@
+import logo from "../../../assets/svgs/logo.svg";
+
+export const sideBarItem = [
+  {
+    id: 1,
+    title: "",
+    items: [
+      {
+        id: "home",
+        icon: logo,
+        name: "Home",
+        subitems: [],
+        link: "/",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "",
+    items: [
+      {
+        id: "subscriber",
+        icon: logo,
+        name: "Subscriber",
+        subitems: [],
+        link: "/",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "",
+    items: [
+      {
+        id: "user",
+        icon: logo,
+        name: "User",
+        subitems: [],
+        link: "/",
+      },
+    ],
+  },
+];
